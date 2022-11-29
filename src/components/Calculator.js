ce3bcf574272099eecfd7f7403bf754f83945090
storres20 <sanchotorres777@gmail.com>
2022-11-29 17:51:54 -0500

    Add events and functionality

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,46 +1,57 @@
 import React from 'react';
+import calculate from '../logic/calculate';
 
 export default class Calculator extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = { total: 0, next: null, operation: null };
+    this.valbutton = this.valbutton.bind(this);
+  }
+
+  valbutton(e) {
+    this.setState((state) => calculate(state, e.target.textContent));
   }
 
   render() {
+    const { total, next, operation } = this.state;
     return (
       <div className="calcContainer">
         <div className="calc">
-          <span className="output">0</span>
+          <span className="output">
+            {total}
+            {operation}
+            {next}
+          </span>
           <div className="horizontalBtn">
             <div className="verticalBtn">
-              <button type="button" className="btnGray">AC</button>
-              <button type="button" className="btnGray">7</button>
-              <button type="button" className="btnGray">4</button>
-              <button type="button" className="btnGray">1</button>
+              <button type="button" className="btnGray" onClick={this.valbutton}>AC</button>
+              <button type="button" className="btnGray" onClick={this.valbutton}>7</button>
+              <button type="button" className="btnGray" onClick={this.valbutton}>4</button>
+              <button type="button" className="btnGray" onClick={this.valbutton}>1</button>
             </div>
             <div className="verticalBtn">
-              <button type="button" className="btnGray">+/-</button>
-              <button type="button" className="btnGray">8</button>
-              <button type="button" className="btnGray">5</button>
-              <button type="button" className="btnGray">2</button>
+              <button type="button" className="btnGray" onClick={this.valbutton}>+/-</button>
+              <button type="button" className="btnGray" onClick={this.valbutton}>8</button>
+              <button type="button" className="btnGray" onClick={this.valbutton}>5</button>
+              <button type="button" className="btnGray" onClick={this.valbutton}>2</button>
             </div>
             <div className="verticalBtn">
-              <button type="button" className="btnGray">%</button>
-              <button type="button" className="btnGray">9</button>
-              <button type="button" className="btnGray">6</button>
-              <button type="button" className="btnGray">3</button>
+              <button type="button" className="btnGray" onClick={this.valbutton}>%</button>
+              <button type="button" className="btnGray" onClick={this.valbutton}>9</button>
+              <button type="button" className="btnGray" onClick={this.valbutton}>6</button>
+              <button type="button" className="btnGray" onClick={this.valbutton}>3</button>
             </div>
             <div className="verticalBtn">
-              <button type="button" className="btnOrange">÷</button>
-              <button type="button" className="btnOrange">*</button>
-              <button type="button" className="btnOrange">-</button>
-              <button type="button" className="btnOrange">+</button>
+              <button type="button" className="btnOrange" onClick={this.valbutton}>÷</button>
+              <button type="button" className="btnOrange" onClick={this.valbutton}>*</button>
+              <button type="button" className="btnOrange" onClick={this.valbutton}>-</button>
+              <button type="button" className="btnOrange" onClick={this.valbutton}>+</button>
             </div>
           </div>
           <div className="lastRow">
-            <button type="button" className="btnGray zero">0</button>
-            <button type="button" className="btnGray">.</button>
-            <button type="button" className="btnOrange">=</button>
+            <button type="button" className="btnGray zero" onClick={this.valbutton}>0</button>
+            <button type="button" className="btnGray" onClick={this.valbutton}>.</button>
+            <button type="button" className="btnOrange" onClick={this.valbutton}>=</button>
           </div>
         </div>
       </div>

79912ab4e267e00f10a693241f76aa720ac9a845
storres20 <sanchotorres777@gmail.com>
2022-12-07 14:08:27 -0500

    Add react-router-dom and import

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import Calculator from './components/Calculator';
+// import Calculator from './components/Calculator';
+import Navbar from './components/Navbar';
+import Calc from './pages/Calc';
+import Home from './pages/Home';
+import NotMatch from './pages/NotMatch';
+import Quote from './pages/Quote';
 
 function App() {
   return (
     <div>
-      <h1>Math Magician Project</h1>
-      <Calculator />
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/calculator" element={<Calc />} />
+        <Route path="/quote" element={<Quote />} />
+        <Route path="*" element={<NotMatch />} />
+      </Routes>
+
     </div>
   );
 }

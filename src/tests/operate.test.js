28d16c9596ec0adac6ad67502c7aa8ae8ed37f17
Giorgi Bokuchava <g.bokuchava22@gmail.com>
2022-12-08 07:41:59 -0800

    Add operate tests

--- src/tests/operate.test.js
+++ src/tests/operate.test.js
@@ -2,19 +2,31 @@ import operate from '../logic/operate';
 
 describe('Testing operate.js', () => {
   test('Plus operation', () => {
-    expect(operate(4, 6, '+')).toBe('10');
+    expect(operate(10, 6, '+')).toBe('16');
   });
 
   test('Minus operation', () => {
     expect(operate(8, 5, '-')).toBe('3');
   });
 
+  test('Minus operation', () => {
+    expect(operate(10, 54, '-')).toBe('64');
+  });
+
   test('Divide operation', () => {
     expect(operate(8, 4, '÷')).toBe('2');
   });
 
+  test('Divide operation', () => {
+    expect(operate(10, 5, '÷')).toBe('2');
+  });
+
   test('Multy operation', () => {
     expect(operate(2, 4, 'x')).toBe('8');
+  });
+
+  test('Multy operation', () => {
+    expect(operate(9, 9, 'x')).toBe('81');
   });
 
   test('Modulo operation', () => {

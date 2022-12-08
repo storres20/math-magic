2c3752b8d4d27127405204a3c07cf5d263b40a14
storres20 <sanchotorres777@gmail.com>
2022-12-07 21:42:04 -0500

    Add Navbar test file

---
+++ src/tests/Navbar.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import renderer from 'react-test-renderer';
+import Navbar from '../components/Navbar';
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(
+      <BrowserRouter>
+        <Navbar />
+      </BrowserRouter>,
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
